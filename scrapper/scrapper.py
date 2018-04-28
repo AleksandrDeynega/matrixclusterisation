a647ba0088a97cc0b1f5b8a9255f7078f6b339ea
AleksandrDeynega <jesse.pinkman.d@gmail.com>
2018-04-28 17:09:18 +0300

    started working on parsing;  data is pretty dirty;

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -8,29 +8,35 @@ def url(country):
     return base_url + "?RP=00{}".format(country)
 
 
+def print_values(soup):
+    rows = soup.find("table", {"id": "RadGrid1_ctl01"}).tbody.findAll("tr")
+    for row in rows:
+        print("{}       {}".format(row.findAll("td")[3].text, row.findAll("td")[4].text))
+
+
 def main():
-    with requests.Session() as session:
-        response = session.get(url(8))
-        # print(response.content)
-        soup = BeautifulSoup(response.content, "lxml")
+    # with requests.Session() as session:
+    session = requests.Session()
+    response = session.get(base_url)
+    # print(response.content)
+    soup = BeautifulSoup(response.content, "lxml")
 
-        data = {'__EVENTTARGET': '',
-                '__EVENTARGUMENT': '',
-                '__LASTFOCUS': '',
-                '__VIEWSTATE': soup.find('input', {'name': '__VIEWSTATE'}).get('value', ''),
-                '__VIEWSTATEGENERATOR': soup.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value', ''),
-                '__EVENTVALIDATION': soup.find('input', {'name': '__EVENTVALIDATION'}).get('value', ''),
-                'TradeComTabs1$RadTabStrip1:': '{"State":{},"TabState":{"TradeComTabs1_RadTabStrip1_tabTPI":{"Selected":true},"TradeComTabs1_RadTabStrip1_tabTPI_tabTPIC":{"Selected":true}}}',
-                'dropReporter': '024',
-                'dropYear': 2016,
-                'lbxSector': '14',
-                'btnRedo': 'Redo'}
+    data = {'__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__LASTFOCUS': '',
+            '__VIEWSTATE': soup.find('input', {'name': '__VIEWSTATE'}).get('value', ''),
+            '__VIEWSTATEGENERATOR': soup.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value', ''),
+            '__EVENTVALIDATION': soup.find('input', {'name': '__EVENTVALIDATION'}).get('value', ''),
+            'TradeComTabs1$RadTabStrip1:': '{"State":{},"TabState":{"TradeComTabs1_RadTabStrip1_tabTPI":{"Selected":true},"TradeComTabs1_RadTabStrip1_tabTPI_tabTPIC":{"Selected":true}}}',
+            'dropReporter': '076',
+            'dropYear': 2016,
+            'lbxSector': '01',
+            'btnRedo': 'Redo'}
 
-        response = session.post(base_url, data=data)
-        #
-        soup = BeautifulSoup(response.content, "lxml")
-        print(soup.text)
+    response = session.post(base_url, data=data)
+    soup = BeautifulSoup(response.content, "lxml")
 
+    print_values(soup)
 
 
 if __name__ == '__main__':
