b4e854a9157d644fcc63ce2da251a4a8dde978b4
AleksandrDeynega <jesse.pinkman.d@gmail.com>
2018-04-29 11:28:06 +0300

    added parsing and df.to_csv(); need improvement of df creation; need to run hole script;

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas
+import logging as log
 
-base_url = "https://tradecompetitivenessmap.intracen.org/TPIC.aspx"
+url = "https://tradecompetitivenessmap.intracen.org/TPIC.aspx"
+sectors = ["%.2d" % i for i in range(1, 4)]
+headers = ['N', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'P1', 'P2', 'P3', 'P4a', 'P4b',
+           'P5a', 'P5b', 'C1', 'C1a', 'C1b', 'C1c', 'C1d', 'C2', 'A', 'P', 'C']
 
-sectors = ["%.2d" % i for i in range(1, 15)]
 
-
-def url(country):
-    return base_url + "?RP=00{}".format(country)
+# def url(country):
+#     return base_url + "?RP=00{}".format(country)
 
 
 def print_values(soup):
@@ -16,35 +19,46 @@ def print_values(soup):
         print("{}       {}".format(row.findAll("td")[3].text, row.findAll("td")[4].text))
 
 
-def get_values(soup):
+def to_array(soup):
     rows = soup.find("table", {"id": "RadGrid1_ctl01"}).tbody.findAll("tr")
-    for row in rows:
-        print("{}       {}".format(row.findAll("td")[3].text, row.findAll("td")[4].text))
+    return [[row.findAll("td")[3].text for row in rows],
+            [row.findAll("td")[4].text for row in rows]]
 
 
 def main():
     with requests.Session() as session:
         session = requests.Session()
-        response = session.get(base_url)
-        # print(response.content)
-        soup = BeautifulSoup(response.content, "lxml")
+        response = session.get(url)
+        data = basic_data(BeautifulSoup(response.content, "lxml"))
+        values_of_country = []
+        country = '076'
         for sector in sectors:
-            data = {'__EVENTTARGET': '',
-                    '__EVENTARGUMENT': '',
-                    '__LASTFOCUS': '',
-                    '__VIEWSTATE': soup.find('input', {'name': '__VIEWSTATE'}).get('value', ''),
-                    '__VIEWSTATEGENERATOR': soup.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value', ''),
-                    '__EVENTVALIDATION': soup.find('input', {'name': '__EVENTVALIDATION'}).get('value', ''),
-                    'TradeComTabs1$RadTabStrip1:': '{"State":{},"TabState":{"TradeComTabs1_RadTabStrip1_tabTPI":{"Selected":true},"TradeComTabs1_RadTabStrip1_tabTPI_tabTPIC":{"Selected":true}}}',
-                    'dropReporter': '076',
-                    'dropYear': 2016,
-                    'lbxSector': sector,
-                    'btnRedo': 'Redo'}
-
-            response = session.post(base_url, data=data)
+            log.info("Parsing sector: %s", sector)
+            response = session.post(url, data=build_data(data, sector, country))
             soup = BeautifulSoup(response.content, "lxml")
-            print_values(soup)
+            values_of_country += to_array(soup)
+        pandas.DataFrame(values_of_country, columns=headers).to_csv("countries/{}".format(country))
+        print(pandas.read_csv("countries/{}".format(country)))
+
+
+def build_data(data, sector, country, year=2016):
+    data['dropReporter'] = country
+    data['dropYear'] = 2016
+    data['lbxSector'] = sector
+    return data
+
+
+def basic_data(soup):
+    return {'__EVENTTARGET': '',
+            '__EVENTARGUMENT': '',
+            '__LASTFOCUS': '',
+            '__VIEWSTATE': soup.find('input', {'name': '__VIEWSTATE'}).get('value', ''),
+            '__VIEWSTATEGENERATOR': soup.find('input', {'name': '__VIEWSTATEGENERATOR'}).get('value', ''),
+            '__EVENTVALIDATION': soup.find('input', {'name': '__EVENTVALIDATION'}).get('value', ''),
+            'TradeComTabs1$RadTabStrip1:': '{"State":{},"TabState":{"TradeComTabs1_RadTabStrip1_tabTPI":{"Selected":true},"TradeComTabs1_RadTabStrip1_tabTPI_tabTPIC":{"Selected":true}}}',
+            'btnRedo': 'Redo'}
 
 
 if __name__ == '__main__':
+    log.basicConfig(level=log.INFO)
     main()
